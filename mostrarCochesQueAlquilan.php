79fd10c8aa4e90009d70b4ebc2504776d25bacdc
joseba-rodriguez <joseba.rodriguez01@gmail.com>
2021-10-15 13:07:58 +0200

    footer

--- mostrarCochesQueAlquilan.php
+++ mostrarCochesQueAlquilan.php
@@ -60,6 +60,25 @@
 
 <a href="index.html"><input type="submit" value="Volver al menu"></a><br>
 <a href="elegirCocheParaEditar.html"><input type="submit" value="Modificar coche"></a>
+
+<div class="footer-basic">
+        <footer>
+         <div class="social">
+                <a href="https://www.instagram.com/"><i class="icon ion-social-instagram" ><img src="Imagenes/instagram.png" alt="" width="40" class="mb-3"></i>
+            </a><a href="https://accounts.snapchat.com/accounts/login?continue=https%3A%2F%2Faccounts.snapchat.com%2Faccounts%2Fwelcome"><i class="icon ion-social-snapchat"><img src="Imagenes/snaptchat.png" alt="" width="40" class="mb-3"></i>
+            </a><a href="https://twitter.com/?lang=es"><i class="icon ion-social-twitter"><img src="Imagenes/twitter.png" alt="" width="40" class="mb-3"></i>
+            </a><a href="https://es-es.facebook.com/"><i class="icon ion-social-facebook"><img src="Imagenes/facebook.png" alt="" width="40" class="mb-3"></i></a>
+        </div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="index.html">Inicio</a></li>
+            <li class="list-inline-item"><a href="index.html">Servicios</a></li>
+            <li class="list-inline-item"><a href="index.html">Sobre nosotros</a></li>
+            <li class="list-inline-item"><a href="index.html">Terminos</a></li>
+            <li class="list-inline-item"><a href="index.html">Política de privacida</a></li>
+        </ul>
+            <p class="copyright">GaJo © 2021</p>
+        </footer>
+    </div>
 </body>
 </html>
 
