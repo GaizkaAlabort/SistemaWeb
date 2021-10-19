338d4cc81d205fa486c39f3a17c4b08a05e7c051
GaizkaAlabort <gaizkalabortupv@gmail.com>
2021-10-19 20:17:57 +0200

    modificar y eliminar coches

--- registroCoche.php
+++ registroCoche.php
@@ -16,21 +16,25 @@
     $caballos = $_POST["caballos"];
     $matricula = $_POST["matricula"];
     $telefono = $_POST["telefono"];
-  
-    
-    $instruccion_SQL="INSERT INTO tablacoches (marca, modelo, caballos, matricula, telefono) VALUES ('$marca','$modelo','$caballos','$matricula','$telefono')";
 
-    if (!mysqli_query($connection, $instruccion_SQL)) {
-        echo"<script>alert('Comprueba lo que has escrito. Puede que te hayas confundido de matricula porque esa ya existe en la lista.');window.location='registraTuCoche.html'</script>";
+    $comprobacion_SQL="SELECT matricula FROM tablacoches WHERE matricula='$matricula'";
+
+    $resultadoComprobacion = mysqli_query($connection, $comprobacion_SQL) or die (mysqli_error($connection));
+
+    if($resultadoComprobacion->num_rows== 0){
+        
+        $resultadoComprobacion->close();
+        $instruccion_SQL="INSERT INTO tablacoches (marca, modelo, caballos, matricula, telefono) VALUES ('$marca','$modelo','$caballos','$matricula','$telefono')";
+
+        $resultado = mysqli_query($connection, $instruccion_SQL) or die (mysqli_error($connection));
+        
+        if(!$resultado){
+            echo"Hubo Algun Error";
+        }else{
+            echo"<script>alert('Se ha registrado correctamente'); window.location='mostrarCochesQueAlquilan.php'</script>";
+        }
+    } else {
+        echo"<script>alert('Coche con esa matricula ya registrada en la web'); window.location='mostrarCochesQueAlquilan.php'</script>";
     }
-
-    $resultado = mysqli_query($connection, $instruccion_SQL) or die (mysqli_error($connection));
-    
-    if(!$resultado){
-        echo"Hubo Algun Error";
-    }else{
-        echo"<script>alert('Se ha registrado correctamente'); window.location='index.html'</script>";
-    }
-
-    
+   
 ?>
